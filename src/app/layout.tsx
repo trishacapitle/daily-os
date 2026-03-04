d714cf62145ab2159e895547edd7f8be374a472d
ashdev <trishacapitle01@gmail.com>
2026-03-04 19:09:51 +0800

    fix: local font not rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${alpinoSans.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${alpinoSans.className} antialiased`}
       >
         <ThemeProvider
           attribute="class"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
+    <div className="flex min-h-screen items-center justify-center">
       <h1 className="font-alpino text-5xl font-black">Daily OS</h1>
     </div>
   );
