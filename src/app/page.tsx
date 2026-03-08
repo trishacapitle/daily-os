1434e2db983507d998ee4e2a752916b188e0ab35
ashdev <trishacapitle01@gmail.com>
2026-03-08 19:38:07 +0800

    feat: add date as dependency for dashboard pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
+// TODO: Add AUTH later
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+// import { ArrowRight } from "lucide-react";
+
+export default async function Home() {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4">
       <h1>
@@ -11,10 +19,12 @@ export default function Home() {
         &quot;Your daily operating system. Build better days.&quot;
       </p>
       <div className="flex gap-6">
-        <Button className="text-base">
+        {/* <Button className="text-base">
           Create an account <ArrowRight />
+        </Button> */}
+        <Button variant="outline" className="text-base">
+          <Link href={`/dashboard/${year}/${month}`}>Continue as guest</Link>
         </Button>
-        <Button variant="outline" className="text-base">Continue as guest</Button>
       </div>
     </div>
   );
