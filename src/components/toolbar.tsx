95b4d824f4cc4b1500e3f84a81b9f68573c0bb39
ashdev <trishacapitle01@gmail.com>
2026-03-08 20:53:42 +0800

    feat: fix dock colors

---
+++ src/components/toolbar.tsx
@@ -0,0 +1,64 @@
+import {
+  Activity,
+  Component,
+  HomeIcon,
+  Mail,
+  Package,
+  ScrollText,
+  SunMoon,
+} from "lucide-react";
+
+import { Dock, DockIcon, DockItem, DockLabel } from "@/components/ui/dock";
+
+const data = [
+  {
+    title: "Home",
+    icon: <HomeIcon className="text-foreground h-full w-full" />,
+    href: "#",
+  },
+  {
+    title: "Products",
+    icon: <Package className="text-foreground h-full w-full" />,
+    href: "#",
+  },
+  {
+    title: "Components",
+    icon: <Component className="text-foreground h-full w-full" />,
+    href: "#",
+  },
+  {
+    title: "Activity",
+    icon: <Activity className="text-foreground h-full w-full" />,
+    href: "#",
+  },
+  {
+    title: "Change Log",
+    icon: <ScrollText className="text-foreground h-full w-full" />,
+    href: "#",
+  },
+  {
+    title: "Email",
+    icon: <Mail className="text-foreground h-full w-full" />,
+    href: "#",
+  },
+  {
+    title: "Theme",
+    icon: <SunMoon className="text-foreground h-full w-full" />,
+    href: "#",
+  },
+];
+
+export function Toolbar() {
+  return (
+    <div className="absolute bottom-2 left-1/2 max-w-full -translate-x-1/2">
+      <Dock className="items-end pb-3">
+        {data.map((item, idx) => (
+          <DockItem key={idx} className="aspect-square rounded-full">
+            <DockLabel>{item.title}</DockLabel>
+            <DockIcon>{item.icon}</DockIcon>
+          </DockItem>
+        ))}
+      </Dock>
+    </div>
+  );
+}
--- src/components/ui/dock.tsx
+++ src/components/ui/dock.tsx
@@ -108,7 +108,7 @@ function Dock({
           mouseX.set(Infinity);
         }}
         className={cn(
-          "mx-auto flex w-fit gap-4 rounded-2xl bg-gray-50 px-4 dark:bg-neutral-900",
+          "bg-card mx-auto flex w-fit gap-4 rounded-2xl",
           className,
         )}
         style={{ height: panelHeight }}
